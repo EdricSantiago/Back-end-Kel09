d915796206e49995ac800effdc9ca528007da512
EdricSan22 <edricksan66@gmail.com>
2026-04-08 03:03:02 +0700

    add account models

---
+++ models/account.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const accountSchema = new mongoose.Schema({
+    accountNumber: {
+        type: String,
+        required: true
+    },
+
+    balance: {
+        type: Number,
+        required: true
+    },
+
+    userid: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+
+},{timestamps: true});
+
+module.exports = mongoose.model('Account', accountSchema);
