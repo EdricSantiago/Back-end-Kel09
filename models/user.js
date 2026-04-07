e1e80c38bade2d7e6d4e2c31a5a54edbb2fb5a27
Kazuhash <Jerichoangdev@gmail.com>
2026-04-07 21:56:07 +0700

    update

--- models/user.js
+++ models/user.js
@@ -1,1 +1,8 @@
-const User = require('./models/User'); 
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    accountNumber: { type: String, required: true },
+    balance: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model('User', userSchema);
