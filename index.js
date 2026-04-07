48a4f276be8442d6db08a8cc652f79b2e048cf4f
Kazuhash <jerichoangdev@gmail.com>
2026-04-07 18:56:10 +0700

    test

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const Bag = require('./models/Bag');
+
+const app = express();
+app.use(express.json());
+const port =3100;
+
+app.get('/', (req,res) =>{
+    res.send('Hello, world');
+});
+
+app.get('/about',(req,res)=>{
+res.send('About me...');
+});
+
+app.listen(port, () => {
+console.log(`App listening on port ${port}`);
+});
