c5211273a8018fe01e50ee72dbfac31637111b85
Kyrexy <loldude.bm@gmail.com>
2026-04-08 12:54:26 +0700

    a

--- controllers/login.js
+++ controllers/login.js
@@ -1,11 +1,45 @@
 const mongoose = require("mongoose");
+const bcrypt= require("bcrypt");
 
-await User.updateOne({
-    username: "luisdewa",
-});
+const register=async(req,res)=>{
+    try{
+        const{username,password}=req.body;
+    
+    const existingUser=await User.findOne({username});
+    if(existingUser){
+        return res.status(400).json({message:"Username sudah terambil"});
+    }
+    const hashedPassword=await bcrypt.hash(password,10);
+    const newUser=await User.create({
+        username,
+        password:hashedPassword,
+    });
+    res.status(201).json({message:"Register Sukses!!",user:newUser});
+    }
+    catch(err){
+    res.status(500).json({message:"Server error",error:err.message});
+    }
+};
 
+const login=async(req,res)=>{
+    try{
+        const{username,password}=req.body;
+    
+        const user=await User.findOne({username});
+        if(!user){
+        return res.status(404).json({message:"User tidak ditemukan"});
+        }
 
-await User.deleteOne({
-    username: "luisdewa",
-    password: "LuissukaPakJansen"
-});
+        const isMatch=await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(401).json({message:"Password salah"});
+        }
+       
+        res.status(200).json({message:"Login berhasil",user});
+    }
+    catch(err){
+        res.status(500).json({message:"Server error",error:err.message});
+    }
+};
+
+module.exports={register,login};
