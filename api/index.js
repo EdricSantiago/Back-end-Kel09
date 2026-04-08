cd22947744902a506510b116237087bf340b052d
Lyinceer <theyxeldev113@gmail.com>
2026-04-09 03:17:43 +0700

    feat: add 2 new routes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
+const login = require('./login-route');
 const users = require('./users-route');
 const account = require('./account-route');
 
 module.exports = () => {
     const app = express.Router();
 
+    login(app); 
     users(app);
     account(app);
 
