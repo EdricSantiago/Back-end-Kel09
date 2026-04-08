2401bf33fcd0d780a77b56d3e7a86d088e9a93c6
Lyinceer <theyxeldev113@gmail.com>
2026-04-09 02:44:25 +0700

    feat: updated

--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose =require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    accountNumber: { type: String, required: true },
-    balance: { type: Number, default: 0 }
+const userSchema=new mongoose.Schema({
+    username:{type:String,required:true,unique:true},
+    password:{type:String,require:true},
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports=mongoose.model("User",userSchema);
